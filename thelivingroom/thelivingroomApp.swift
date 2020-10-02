13482c62aaabb7062dd8e635a9d03ace3fe01a2a
Omar Hammad <omar@aqwas.sa>
2020-10-02 09:05:22 -0600

    Initial Commit

---
+++ thelivingroom/thelivingroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  thelivingroomApp.swift
+//  thelivingroom
+//
+//  Created by Omar on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct thelivingroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
