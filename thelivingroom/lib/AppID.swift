daf3468b07269212a3b547bdb83a94fbb9d148ff
Omar <hammado92@gmail.com>
2020-10-06 18:42:07 -0600

    Update AppID.swift

--- thelivingroom/lib/AppID.swift
+++ thelivingroom/lib/AppID.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Agora.io. All rights reserved.
 //
 
-let AppID: String = "e894803468094df9b69474d7535f1aa7"
+let AppID: String = "YOUR_APP_ID"
 /* assign Token to nil if you have not enabled app certificate
  * before you deploy your own token server, you can easily generate a temp token for dev use
  * at https://dashboard.agora.io note the token generated are allowed to join corresponding room ONLY.
@@ -15,4 +15,4 @@ let AppID: String = "e894803468094df9b69474d7535f1aa7"
  * 生成Token需要参照官方文档部署Token服务器，开发阶段若想先不部署服务器, 可以在https://dashbaord.agora.io生成
  * 临时Token. 请注意生成Token时指定的频道名, 该Token只允许加入对应的频道
  */
-let Token: String? = "006e894803468094df9b69474d7535f1aa7IADzR63LZ4gqUxr3Yn6UJD2m20OQirub+8mCk3xiyyB2498AXuMAAAAAEAANYBkGY0J+XwEAAQBjQn5f"
+let Token: String? = "YOUR_TOKEN"
